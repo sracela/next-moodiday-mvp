ee58208721a129383eb8a0d06a39c7dc146cac2b
Sara Cela <sara.cela@innovamat.com>
2023-07-30 05:00:07 +0200

    STYLES - fix main layout

--- app/components/MainSection.tsx
+++ app/components/MainSection.tsx
@@ -47,7 +47,7 @@ export default async function MainSection({
           />
         )}
       </div>
-      <div className="flex flex-col py-2 gap-6">
+      <div className="container mx-auto flex flex-col py-2 gap-6">
         {page.data.map((section: any) => (
           <div key={section.id}>
             <h2 className="py-2 section-heading">
