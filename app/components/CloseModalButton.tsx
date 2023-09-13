9e8da6cba281c6e0fa2dc19216700c6a79cfeb6f
Sara Cela <sara.cela@innovamat.com>
2023-09-12 19:13:35 -0700

    FIX - closing modla when no history state

--- app/components/CloseModalButton.tsx
+++ app/components/CloseModalButton.tsx
@@ -5,8 +5,15 @@ import { MdClose } from "react-icons/md";
 
 export default function CloseModalButton() {
   const router = useRouter();
+  const handleBack = () => {
+    if (window.history.state && window.history.state.idx > 0) {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  };
   return (
-    <button className="close-btn" onClick={router.back}>
+    <button className="close-btn" onClick={handleBack}>
       <MdClose size={20} />
     </button>
   );
