6ee29ae2c293948008528918626a0845d8138ab2
anpave <anthonyvelizlandazuri@gmail.com>
2023-09-21 16:23:47 -0500

    add no result in search page and fix skeleton

--- app/search/[query]/layout.tsx
+++ app/search/[query]/layout.tsx
@@ -6,7 +6,7 @@ export default function PageLayout({
   parallel: React.ReactNode;
 }) {
   return (
-    <section className="min-h-screen">
+    <section className="mb-auto mt-auto">
       {children}
       {parallel}
     </section>
