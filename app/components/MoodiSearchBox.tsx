fa4d320674ce1d892b69e63d9c00b6a4444d2821
anpave <anthonyvelizlandazuri@gmail.com>
2023-09-22 18:33:16 -0500

    modify width of search bar according the display

--- app/components/MoodiSearchBox.tsx
+++ app/components/MoodiSearchBox.tsx
@@ -7,7 +7,7 @@ import { SearchBox } from "react-instantsearch";
 export default function MoodiSearchBox() {
   const router = useRouter();
   return (
-    <div className="w-full px-4 py-2 fixed top-0 z-20 pointer-events-none">
+    <div className="w-1/3 sm:w-1/2 md:w-full px-4 py-2 fixed top-0 right-0 z-20 pointer-events-none">
       <div className="container flex justify-end h-16 mx-auto px-0 sm:px-6">
         <div className="searchBox pointer-events-auto">
           <SearchBox
